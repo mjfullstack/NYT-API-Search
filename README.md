9d7a990cf9d79cff8f07074969dcbe266d8a5678
MJ FullStack <mjohnson3@cox.net>
2018-09-18 15:49:05 -0700

    Added api call on search click. Still need dates, correct number of items, etc.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NYT-API-Search
 New York Times API Search
+
+*09/18/18:  Added api call on search click. Still need dates, correct number of items, etc
+
--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,30 +1,67 @@
+$(document).ready(function(){
+
+function createJQuery (num, response) {
+  for (i=0; i<num; i++) {
+
+  screenWell = $("<div>").attr("class", "well well-lg");
+  screenWell.attr("id", "well-" + i);
+  //  Title
+  itemText = "<h3>Title: " + response.response.docs[i].headline.main + "</h3>";
+  //  Author
+  itemText += "<h4>Author: " + response.response.docs[i].byline.original + "</h4>";
+  //  Section
+  itemText += "<h4>Section: " + response.response.docs[i].headline.kicker + "</h4>";
+  //  Date
+  itemText += "<h4>Publication Date: " + response.response.docs[i].pub_date + "</h4>";
+  //  Link
+  itemText += "<span>Link: </span><a href='response.response.docs[i].web_url'>" + response.response.docs[i].web_url + "</a>";
+
+  screenWell.html(itemText);
+  $(".card-body").append(screenWell);
+  }
+
+};
 
 
+  $("#search-btn").on("click", function () {
+    
 
+    var mySearchString = $("#q-search-term").val().trim(); // id="num-of-reccords"
+    var mySearchNumOfRecords = $("#num-of-reccords").val().trim(); // id="num-of-reccords"
+    console.log("mySearchString = " + mySearchString);
+    // console.log("mySearchString = " + mySearchString)
 
-var mySearchString = $("#q-search-term").text(); // id="num-of-reccords"
-var mySearchNumOfRecords = $("#num-of-reccords").val(); // id="num-of-reccords"
-console.log("mySearchString = " + mySearchString);
-// console.log("mySearchString = " + mySearchString)
+    var mySearchStartDate = " ";
+    if ($("#start-date").val()) {
+      mySearchStartDate = $("#start-date").val().trim();
+    };
+    
+    var mySearchEndDate = " ";
+    if ($("#end-date").val()) {
+      var mySearchEndDate = $("#query-term").val().trim();
+    };
 
-var mySearchStartDate = $("#query-term").val();
-var mySearchEndDate = $("#query-term").val();
-
-// Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "26cdc544d1c44879a6e42b3a988987ba",
-  'q': mySearchString,
-//   'num': mySearchNumOfRecords,
-  'begin_date' : mySearchStartDate,
-  'end_date' : mySearchEndDate
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-// })
-// .fail(function(err) {
-//   throw err;
+    // Built by LucyBot. www.lucybot.com
+    var urlBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url = urlBase + '?' + $.param({
+      // 'api-key': "26cdc544d1c44879a6e42b3a988987ba",
+      'api-key': "88fdb158c84640dcbe91c397193844b7",
+      'q': mySearchString,
+    //   'num': mySearchNumOfRecords,
+      // 'begin_date' : mySearchStartDate,
+      // 'end_date' : mySearchEndDate
+    });
+    console.log (url);
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+      // console.log(JSON.parse(result));
+      createJQuery(3 , result)
+    })
+    .fail(function(err) {
+      throw err;
+    });
+  });
 });
