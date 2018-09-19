ce60ea262fd65260eb211a2d7f14ce8b01214cbf
MJ FullStack <mjohnson3@cox.net>
2018-09-19 11:12:57 -0700

    Added results to individual cards. Still to go, years, numArticles input and nulls.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # NYT-API-Search
 New York Times API Search
 
-*09/18/18:  Added api call on search click. Still need dates, correct number of items, etc
+*09/18/18:  Added api call on search click. Still need dates, correct number of items, etc.
+*09/19/18 Added results to individual cards. Still to go, years, numArticles input and nulls.
 
--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -3,21 +3,21 @@ $(document).ready(function(){
 function createJQuery (num, response) {
   for (i=0; i<num; i++) {
 
-  screenWell = $("<div>").attr("class", "well well-lg");
-  screenWell.attr("id", "well-" + i);
-  //  Title
-  itemText = "<h3>Title: " + response.response.docs[i].headline.main + "</h3>";
-  //  Author
-  itemText += "<h4>Author: " + response.response.docs[i].byline.original + "</h4>";
-  //  Section
-  itemText += "<h4>Section: " + response.response.docs[i].headline.kicker + "</h4>";
-  //  Date
-  itemText += "<h4>Publication Date: " + response.response.docs[i].pub_date + "</h4>";
-  //  Link
-  itemText += "<span>Link: </span><a href='response.response.docs[i].web_url'>" + response.response.docs[i].web_url + "</a>";
+    screenCard = $("<div>").attr("class", "card");
+    screenCard.attr("id", "card-body-" + i);
+    //  Title
+    itemText = "<h3>Title: " + response.response.docs[i].headline.main + "</h3>";
+    //  Author
+    itemText += "<h4>Author: " + response.response.docs[i].byline.original + "</h4>";
+    //  Section
+    itemText += "<h4>Section: " + response.response.docs[i].headline.kicker + "</h4>";
+    //  Date
+    itemText += "<h4>Publication Date: " + response.response.docs[i].pub_date + "</h4>";
+    //  Link
+    itemText += "<span>Link: </span><a href='response.response.docs[i].web_url'>" + response.response.docs[i].web_url + "</a>";
 
-  screenWell.html(itemText);
-  $(".card-body").append(screenWell);
+    screenCard.html(itemText);
+    $("#card-section").append(screenCard);
   }
 
 };
